816edc8b32a97b06067c302bebcd3e6997419be1
Abby Olaleye <30451506+aolaleye@users.noreply.github.com>
2018-06-25 10:47:56 -0400

    created tables and imported csv files

--- SQL/vet-challenge.sql
+++ SQL/vet-challenge.sql
@@ -6,3 +6,57 @@
 -- 3. Match up all procedures performed to their descriptions
 -- 4. Same as above but only for the pets from the clinic in question
 -- 5. Extract a table of individual costs (procedure prices) incurred by owners of pets from the clinic in question (this table should have owner and procedure price side-by-side)
+
+-------------------------------------------------
+-- Create Tables / Import Data
+-------------------------------------------------
+-- create pets table
+CREATE TABLE pets (
+    petid varchar,
+    name varchar,
+    kind varchar,
+    gender varchar,
+    age int,
+    ownerid varchar
+);
+
+-- import Pets.csv file
+COPY pets FROM '/users/iolaleye/Downloads/database/Pets.csv' DELIMITER ',' CSV HEADER;
+
+-- create owners table
+CREATE TABLE owners (
+    ownerid varchar,
+    name varchar,
+    surname varchar,
+    streetaddress varchar,
+    city varchar,
+    state varchar(2),
+    statefull varchar,
+    zipcode varchar
+);
+
+-- import Owners.csv file
+COPY owners FROM '/users/iolaleye/Downloads/database/Owners.csv' DELIMITER ',' CSV HEADER;
+
+-- create proceduredetails table
+CREATE TABLE proceduredetails (
+    proceduretype varchar,
+    proceduresubcode varchar,
+    description varchar,
+    price float
+);
+
+-- import proceduredetails.csv file
+COPY proceduredetails FROM '/users/iolaleye/Downloads/database/proceduredetails.csv' DELIMITER ',' CSV HEADER;
+
+-- create procedurehistory table
+CREATE TABLE procedurehistory (
+    petid varchar,
+    proceduredate date,
+    proceduretype varchar,
+    proceduresubcode varchar
+);
+
+-- import procedurehistory.csv file 
+COPY procedurehistory FROM '/users/iolaleye/Downloads/database/procedurehistory.csv' DELIMITER ',' CSV HEADER;
+
